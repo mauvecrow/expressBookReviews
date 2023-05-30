2f46a78f4d7e7abc7c5404eb15c18c3b73600f72
Bradley Quangson <bradley.quangson@gmail.com>
2023-05-30 14:04:15 -0600

    moved utility fucntions to other file

--- final_project/index.js
+++ final_project/index.js
@@ -30,60 +30,6 @@ app.use("/customer/auth/*", function auth(req, res, next) {
   }
 });
 
-// ------------------- Utility functions for authentication/authorization ------------------------------------
-
-//list of registered users (following the pattern from the practice lab), type: [{username, password}]
-const users = [];
-
-/*
-    Register new users - POST
-    body expects {username, password}
-*/
-app.post("/register", (req, res) => {
-  const p_username = req.body.username;
-  const p_password = req.body.password;
-  // check if user is in user list based on username
-  const isExistingUser = users.find((user) => user.username === p_username);
-  if (!isExistingUser) {
-    //user not in list
-    users.push({ username: p_username, password: p_password }); //add new user
-    res
-      .status(200)
-      .json({ message: "User successfully registered. You can now login!" });
-  } else {
-    return res.status(404).json({ message: "User already exists!" });
-  }
-});
-
-/*
-    Login with registered user - POST
-    body expects {username, password}
-*/
-app.post("/login", (req, res) => {
-  const p_username = req.body.username;
-  const p_password = req.body.password;
-
-  if (!p_username || !p_password) {
-    return res
-      .status(404)
-      .json({ message: "Please enter both username and password!" });
-  }
-
-  const isValidUser = users.find(
-    (user) => user.username === p_username && user.password === p_password
-  );
-
-  if (isValidUser) {
-    let accessToken = jwt.sign({ data: p_password }, "access", {
-      expiresIn: 60 * 60,
-    });
-    req.session.authorization = { accessToken, p_username };
-    res.status(200).send("User successfully logged in");
-  } else {
-    res.status(204).send("Incorrect credentials!");
-  }
-});
-// ------------------------ END utility functions for authentication/authorization ---------------------------------
 const PORT = 5000;
 
 app.use("/customer", customer_routes);
